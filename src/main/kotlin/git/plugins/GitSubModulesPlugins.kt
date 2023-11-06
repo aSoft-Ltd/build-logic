4ea7ebf1283ad025d76ad27f404b49db3fa019a6
andylamax <andylamax@programmer.net>
2023-11-07 02:40:01 +0300

    before mergin

--- src/main/kotlin/git/plugins/GitSubModulesPlugins.kt
+++ src/main/kotlin/git/plugins/GitSubModulesPlugins.kt
@@ -1,23 +1,52 @@
 package git.plugins
 
-import git.tasks.gitAdd
-import git.tasks.gitCommit
-import git.tasks.gitFetch
-import git.tasks.gitStatus
+import git.tasks.GitAddTask
+import git.tasks.GitCommitTask
+import git.tasks.GitFetchTask
+import git.tasks.GitMergeTask
+import git.tasks.GitStatusTask
 import java.io.File
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.register
 
 class GitSubModulesPlugins : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
-        val modules = submodules()
-        gitStatus(modules)
-        gitAdd(modules)
-        gitCommit(modules)
-        gitFetch(modules)
+        val mods = submodules()
+        val build = layout.buildDirectory
+        tasks.register<GitStatusTask>("gitStatus") {
+            modules.set(mods)
+            destination.set(build.dir("git/status"))
+        }
+        tasks.register<GitAddTask>("gitAdd") {
+            modules.set(mods)
+            destination.set(build.dir("git/add"))
+        }
+        tasks.register<GitCommitTask>("gitCommit") {
+            mustRunAfter("gitAdd")
+            modules.set(mods)
+            message.set(providers.gradleProperty("message"))
+            destination.set(layout.buildDirectory.dir("git/commit"))
+        }
+        tasks.register<GitFetchTask>("gitFetch") {
+            modules.set(mods)
+            from.set(providers.gradleProperty("from"))
+            destination.set(layout.buildDirectory.dir("git/fetch"))
+        }
+        tasks.register<GitMergeTask>("gitMerge") {
+            modules.set(mods)
+            from.set(providers.gradleProperty("from"))
+            destination.set(layout.buildDirectory.dir("git/fetch"))
+        }
+        Unit
     }
 
-    fun Project.submodules() = layout.projectDirectory.asFile.listFiles { file ->
-        file.isDirectory && File(file, ".git").exists()
-    } ?: emptyArray()
+    fun Project.submodules() = buildList{
+        val root = layout.projectDirectory
+        val subs = root.asFile.listFiles { file ->
+            file.isDirectory && File(file, ".git").exists()
+        }
+        addAll(subs.toList())
+        add(root.asFile)
+    }
 }
---
+++ src/main/kotlin/git/tasks/GitMergeTask.kt
@@ -0,0 +1,28 @@
+package git.tasks
+
+import git.models.GitProcess
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+
+abstract class GitMergeTask : GitModuleTask() {
+
+    @get:Input
+    abstract val from: Property<String>
+
+    init {
+        git("merge", from.map { "origin/$it" })
+    }
+
+    override fun finish(processes: List<GitProcess>) = processes.forEach {
+        val outText = it.out.readText()
+        val errText = it.err.readText()
+        if (!outText.contains("Already up to date") && outText.isNotBlank()) {
+            println(it.workdir.absolutePath)
+            println(outText)
+        }
+        if (errText.isNotBlank()) {
+            println(it.workdir.absolutePath)
+            System.out.println(it.out.readText())
+        }
+    }
+}

738e76d54e214c05fc00fbce7ee38aeba8d3a475
andylamax <andylamax@programmer.net>
2023-11-07 03:13:10 +0300

    testing a new commit

--- src/main/kotlin/git/plugins/GitSubModulesPlugins.kt
+++ src/main/kotlin/git/plugins/GitSubModulesPlugins.kt
@@ -27,36 +27,35 @@ class GitSubModulesPlugins : Plugin<Project> {
             dependsOn(add)
             modules.set(mods)
             message.set(providers.gradleProperty("message"))
-            destination.set(layout.buildDirectory.dir("git/commit"))
+            destination.set(build.dir("git/commit"))
         }
         val fetch = tasks.register<GitFetchTask>("gitFetch") {
-            mustRunAfter(add,commit)
+            mustRunAfter(add, commit)
             modules.set(mods)
             from.set(providers.gradleProperty("from"))
-            destination.set(layout.buildDirectory.dir("git/fetch"))
+            destination.set(build.dir("git/fetch"))
         }
         val merge = tasks.register<GitMergeTask>("gitMerge") {
             dependsOn(fetch)
             modules.set(mods)
             from.set(providers.gradleProperty("from"))
-            destination.set(layout.buildDirectory.dir("git/merge"))
+            destination.set(build.dir("git/merge"))
         }
         tasks.register<GitPushTask>("gitPush") {
             mustRunAfter(merge)
             modules.set(mods)
             src.set(providers.gradleProperty("src"))
             dst.set(providers.gradleProperty("dst"))
-            destination.set(layout.buildDirectory.dir("git/push"))
+            destination.set(build.dir("git/push"))
         }
         Unit
     }
 
-    fun Project.submodules() = buildList{
+    private fun Project.submodules() = buildList {
         val root = layout.projectDirectory
-        val subs = root.asFile.listFiles { file ->
+        root.asFile.listFiles { file ->
             file.isDirectory && File(file, ".git").exists()
-        }
-        addAll(subs.toList())
+        }?.toList()?.let { addAll(it) }
         add(root.asFile)
     }
 }

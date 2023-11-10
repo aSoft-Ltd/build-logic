aa587d604325a80cb11b65c3ce209c4d5ba100f1
andylamax <andylamax@programmer.net>
2023-11-11 02:48:34 +0300

    Removed test cases

--- src/test/resources/types/keep.d.ts
+++
@@ -1,5 +0,0 @@
-export declare namespace keep {
-    interface Cacheable {
-        readonly cache: any/* keep.Cache */;
-    }
-}
--- src/test/resources/types/krest.d.ts
+++
@@ -1,9 +0,0 @@
-import { cinematic } from './cinematic'
-
-export declare namespace krest {
-    class WorkerLedger {
-        get type(): string;
-        get topic(): Nullable<string>;
-        get progress(): cinematic.MutableLiveMap<string, kase.ExecutorState<Nullable<any>>>;
-    }
-}

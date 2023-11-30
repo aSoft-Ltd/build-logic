9580a5c35aa36c8662cd02bf8520688b769c20b3
andylamax <andylamax@programmer.net>
2023-11-30 04:36:49 +0300

    Updated gradle from 8.4 -> 8.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,7 @@
-pluginManagement {
-    enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-    repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        mavenCentral()
-        google()
-        gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        File(rootDir.parentFile, "versions/gradle/versions").listFiles().map {
-            it.nameWithoutExtension to it.absolutePath
-        }.forEach { (name, path) ->
-            create(name) { from(files(path)) }
-        }
-    }
-}
+//pluginManagement {
+//    repositories {
+//        gradlePluginPortal()
+//    }
+//}
 
 rootProject.name = "build-logic"

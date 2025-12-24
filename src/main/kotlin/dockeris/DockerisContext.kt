6263864c4a3cc97c9a2b471fa01b696beb8f60b1
andylamax <andylamax@programmer.net>
2025-12-24 09:48:14 +0300

    Fixed regression in port exposure for different environments

--- src/main/kotlin/dockeris/DockerisContext.kt
+++ src/main/kotlin/dockeris/DockerisContext.kt
@@ -15,5 +15,9 @@ data class DockerisContext(
         val db = app + 1
     }
 
-    val port by lazy { OpenPort(app = "${owner.uid}${project.uid}${environment.ordinal}".toInt()) }
+    val port by lazy {
+        // We are multiplying by 2 so as we expose 2 ports (one for the server app and one for the database)
+        // It's okay to potentially increase this number when we feel like we will be exposing more ports (i.e. the SPA browser)
+        OpenPort(app = "${owner.uid}${project.uid}${2 * environment.ordinal}".toInt())
+    }
 }
